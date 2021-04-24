4bbda0c931af68074fe9cd97cff194193179d05e
Berker Özer <me@berkerozer.com.tr>
2021-04-24 20:20:00 +0300

    ejs added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path')
+var expressLayouts = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000;
+require('./src/config/mongoConnect')
+
+const auth = require('./src/middlewares/auth')
+const siteRouter = require('./src/routers/')
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/views'));
+app.use(expressLayouts);
 app.use(express.static('public'))
 
-app.get('',(req,res) => {
-  res.send('hello express');
-});
+app.use('/', auth, siteRouter);
 
 app.get('*', (req,res) => {
   res.sendFile('./public/404.html', { root: __dirname })
